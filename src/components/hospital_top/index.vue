d4179dabee73fd29155b09c38bd2feb64e25b1e3
± <2696735300@qq.com>
2023-09-27 20:16:16 +0800

    顶部和底部样式完成

---
+++ src/components/hospital_top/index.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="placeholder"></div>
+  <div class="top">
+    <div class="content">
+      <div class="left">
+        <img src="../../assets/images/logo.png">
+        <span>医疗通 预约挂号统一平台</span>
+      </div>
+      <div class="right">
+        <span class="help">帮助中心</span>
+        <span class="login">登陆/注册</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style lang="scss">
+.placeholder {
+  height: 70px/* 设置为被固定元素的高度 */;
+}
+.top{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 70px;
+  background: #ffffff;
+  display: flex;
+  justify-content: center;
+  .content{
+    width: 1200px;
+    height: 70px;
+    display: flex;
+    justify-content: space-between;
+    .left{
+      display: flex;
+      align-items: center;
+      font-size: 20px;
+      color: #557bb8;
+      img{
+        height: 55px;
+      }
+    }
+    .right{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 15px;
+      color: #666666;
+      .help{
+        margin-right: 20px;
+      }
+    span:hover{
+      color: #5a92eb;
+    }
+    }
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/style/reset.scss'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+// 引入全局组件
+import HospitalTop from '@/components/hospital_top/index.vue'
+import HospitalBottom from '@/components/hospital_bottom/index.vue'
+
+const app = createApp(App);
+
+// 注册全局组件
+app.component('HospitalTop',HospitalTop)
+app.component('HospitalBottom',HospitalBottom)
+
+app.mount('#app')
